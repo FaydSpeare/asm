8cbe55f66302f090d8cdd1b4ed78be200349deb7
FaydSpeare <fspea123@gmail.com>
2023-06-18 07:01:10 +0200

    stos

--- main.asm
+++ main.asm
@@ -1,34 +1,30 @@
-extern string_to_int
-extern int_to_string
-
 section .data
-	phrase db "sys_gettimeofday_secs: ", 0
-	phrase_len equ $ - phrase
-	buffer db 20 DUP (0)
-	buffer_len equ 20
-	timeval_struct:
-		timeval_sec dq 0  ; seconds
-		timeval_usec dq 0 ; microseconds
+	buffer db 50 DUP (0)
+	buffer_len equ $ - buffer
 
 section .text
 	global _start
 
 _start:	
-	mov rdi, timeval_struct
-	mov rax, 96
-	syscall
+	mov edi, buffer
+	mov al, 70
+	mov ah, 10
+	mov rcx, 25
+	rep stosw
 
-	mov rax, qword [timeval_struct]
-	mov rbx, buffer
-	call int_to_string
-	
-	mov byte [buffer + buffer_len - 1], 10	
+	mov rdi, buffer+16
+	mov rax, ' h ... w'
+	mov al, 10
+	ror rax, 8
+	mov rcx, 3
+	rep stosq
 
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, phrase
-	mov rdx, phrase_len
-	syscall
+	mov rdi, buffer
+	mov eax, " MMM"
+	mov al, 10,
+	ror eax, 8
+	mov rcx, 4
+	rep stosd
 
 	mov rax, 1
 	mov rdi, 1
---
+++ sys_gettimeofday.asm
@@ -0,0 +1,38 @@
+section .data
+	phrase db "sys_gettimeofday_secs: ", 0
+	phrase_len equ $ - phrase
+	buffer db 20 DUP (0)
+	buffer_len equ 20
+	timeval_struct:
+		timeval_sec dq 0  ; seconds
+		timeval_usec dq 0 ; microseconds
+
+section .text
+	global _start
+
+_start:	
+	mov rdi, timeval_struct
+	mov rax, 96
+	syscall
+
+	mov rax, qword [timeval_struct]
+	mov rbx, buffer
+	call int_to_string
+	
+	mov byte [buffer + buffer_len - 1], 10	
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, phrase
+	mov rdx, phrase_len
+	syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, buffer
+	mov rdx, buffer_len
+	syscall
+	
+	mov rdi, 0
+	mov rax, 60
+	syscall
